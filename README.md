ca16b6b5db55e8b10a641fca96868e9357ef68d6
ryan <ryan.carnegie@gmail.com>
2023-05-04 23:17:22 -0700

    new updates

---
+++ README.md
@@ -0,0 +1,6 @@
+# Personal-Website-v2
+A site to demonstrate my past work as well as my resume
+
+This is a revision from v1 edition, but this edition will soon add a strong javascript presence. 
+
+https://ryancarnegie1.web.app/
---
+++ public/scripts/script.js
@@ -0,0 +1,52 @@
+const js_message = document.getElementById("javascript_message").innerHTML = "welcome";
+
+// const ok = document.getElementById('ok');
+// let html;
+
+// let firstString = "bob";
+// let secondString = "    billy";
+
+// html += `<ul>`;
+// html += `<li>Content</li>`;
+// html += `<li>Content</li>`;
+// html += `<li>Content</li>`;
+// html += `<li>Content</li>`;
+// html += `</ul>`;
+
+// ok.innerHTML += html;
+
+// const cars = [];
+// const keys = [true, false, true, true];
+// cars[0] = 'rock';
+// cars[1] = 'paper';
+// cars[2] = 'scissors';
+// const boats = ['ship', 'dock', keys, cars[0], cars[1], cars[2]];
+
+// ok.innerHTML += `<p>My array: ${boats}</p>`;
+// boats.push("Jazzy Jeff");
+// boats.push("Joe Jack");
+
+// ok.innerHTML += `<p>My array: ${boats}</p>`;
+// ok.innerHTML += `<p>My array: ${boats.length}</p>`;
+// const shifted = boats.shift();
+// ok.innerHTML += `<p>My array: ${boats.length}</p>`;
+// ok.innerHTML += shifted;
+// boats[3] = null;
+// let newArray = boats.filter((x) => x);
+// ok.innerHTML += `<p>My array: ${newArray}</p>`;
+// ok.innerHTML += `<p>My array: ${newArray.length}</p>`;
+// for (let index = 1; index <= 10; index++) {
+//   html += `<li>${index}</li>`;
+// }
+// html += `</ul>`
+// ok.innerHTML += html;
+
+// html = `<ul>`;
+// ok.innerHTML += `<p>break line </p>`;
+// for (let i = 0; i < boats.length; i++) {
+//   html += `<li>${boats[i]}</li>`;
+// }
+// html += `</ul>`;
+
+// ok.innerHTML += html;
+
